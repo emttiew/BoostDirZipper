d78f1dfa471f81d08b431f5f6c837d4bcf7add6d
Mateusz Wozniak <emttiew@gmail.com>
2023-12-12 23:17:28 +0100

    compression of multiple files into a single file

--- src/archiveUtils/Compress.cpp
+++ src/archiveUtils/Compress.cpp
@@ -16,36 +16,39 @@ namespace archive_utils
     {
         io::filtering_ostream out;
         out.push(io::gzip_compressor());
-        out.push(io::file_descriptor_sink(outputDir.string()));
 
-        out << "test" << std::endl;
+        for (fs::recursive_directory_iterator it(inputDir), end; it != end; ++it)
+        {
+            if (fs::is_regular_file(*it))
+            {
+                std::cout << "file path: " << it->path().string() << std::endl;
+                std::ifstream file(it->path().string(), std::ios::binary);
+                if (file)
+                {
+                    out.push(io::file_descriptor_sink(outputDir.string(), std::ios::binary | std::ios::app));
+                    std::streamsize bytes_copied = io::copy(file, out);
+                    std::cout << "Compression: Number of bytes copied: " << bytes_copied << std::endl;
+                    file.close();
+                }
+                else
+                    std::cout << "error opening file: " << it->path().string() << std::endl;
+            }
+            if (fs::is_directory(*it))
+                std::cout << "dirctory path: " << it->path().string() << std::endl;
+        }
 
-        // for (fs::recursive_directory_iterator it(inputDir), end; it != end; ++it)
-        // {
-        //     if (fs::is_regular_file(*it))
-        //     {
+        std::cout << "out size: " << out.size() << std::endl;
+
         //         fs::path relativePath = fs::relative(it->path(), inputDir);
         //         std::cout << "relativePath: " << relativePath.string() << std::endl;
-        //         // out << relativePath.string() << '\n';
-
-        //         std::fstream file;
-        //         file.open(it->path().string(), std::ios::out);
-        //         if (file)
-        //         {
-        //             io::copy(file, out);
-        //             file.close();
-        //         }
-        //         else
-        //             std::cout << "error opening file: " << relativePath.string() << std::endl;
-        //     }
-        // }
+        //         out << relativePath.string() << '\n';
     }
 
     void decompressDirectory(const fs::path &inputDir, const fs::path &outputDir)
     {
         io::filtering_istream in;
         in.push(io::gzip_decompressor());
-        in.push(io::file_descriptor_source(inputDir.string()));
+        in.push(io::file_descriptor_source(inputDir.string(), std::ios::binary));
         std::fstream out;
         out.open(outputDir.string(), std::ios::out | std::ios::binary);
 
@@ -54,7 +57,12 @@ namespace archive_utils
             // std::string text;
             // std::getline(in, text);
             // out << text << std::endl;
-            io::copy(in, out);
+            std::streamsize bytes_copied = io::copy(in, out);
+            std::cout << "Decompression: Number of bytes copied: " << bytes_copied << std::endl;
+        }
+        else
+        {
+            std::cout << "Decompression: Error opening file" << std::endl;
         }
 
         // int i = 1;
