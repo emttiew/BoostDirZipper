93ca62710dee5c42b4bb018d08fb0bd8317e5c2e
Mateusz Wozniak <emttiew@gmail.com>
2024-02-18 22:17:48 +0100

    Add simple usage functionality in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,34 @@
-#include "archiveUtils/ArchiveUtils.hpp"
 #include <filesystem>
 #include <iostream>
 
-int main()
-{
-    namespace fs = std::filesystem;
-    const fs::path inputDirectory = "/home/mateusz/Dokumenty/Cpp_projects/compressor_sandbox/BackToTheFutureNew/build/kupa";
-    const fs::path compressedFile = "compressed_file.gzip";
-    const fs::path decompressedDirectory = "/home/mateusz/Dokumenty/Cpp_projects/compressor_sandbox/BackToTheFutureNew/build/kupa2";
-    archive_utils::compressDirectory(inputDirectory, compressedFile);
-    archive_utils::decompressDirectory(compressedFile, decompressedDirectory);
+#include "archiveUtils/ArchiveUtils.hpp"
+
+int main(int argc, char *argv[]) {
+  namespace fs = std::filesystem;
+  if (argc < 3) {
+    std::cout << "Usage: " << argv[0]
+              << " c|compress|d|decompress <source_path> <sink_path>\n";
+    return 1;
+  }
+
+  std::string mode = argv[1];
+  std::string sourcePath = argv[2];
+  std::string archivePath = argv[3];
+
+  if (mode == "c" || mode == "compress") {
+    fs::path source(sourcePath);
+    fs::path archive(archivePath);
+    archive_utils::compressDirectory(source, archive);
+    std::cout << "Compression completed.\n";
+  } else if (mode == "d" || mode == "decompress") {
+    fs::path archive(sourcePath);
+    fs::path destination(archivePath);
+    archive_utils::decompressDirectory(archive, destination);
+    std::cout << "Decompression completed.\n";
+  } else {
+    std::cout << "Invalid mode. Available modes: compress, decompress\n";
+    return 1;
+  }
+
+  return 0;
 }
